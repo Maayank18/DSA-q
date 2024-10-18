9fee7e5e6b10b6271cb1e160a44f67f5d84ac1e1
Maayank18 <maayank51518@gmail.com>
2024-10-18 23:36:17 +0530

    GCD_HCF

--- GCD_HCF.cpp
+++ GCD_HCF.cpp
@@ -1,1 +1,17 @@
-#include
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n,m;
+    cin>>n;
+    cout<<endl;
+    cin>>m;
+
+    for(int i = min(n,m);i>=1; i--){
+        if(n%i==0 && m%i==0){
+            cout<<" The GCD is :"<<i;
+            break;
+        }
+    }
+    return 0;
+}
