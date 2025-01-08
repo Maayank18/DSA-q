f055512dcca78e7ba153d11de194f2b11192d2ad
Maayank18 <maayank51518@gmail.com>
2025-01-08 20:18:12 +0530

    reversing the doubly linked list

--- Linked List/Reversing_DLL.cpp
+++ Linked List/Reversing_DLL.cpp
@@ -5,8 +5,80 @@
 
 
 
+// #include<iostream>
+// #include<stack>
+// using namespace std;
+
+// struct node{
+//     int data;
+//     node * next;
+//     node * prev;
+    
+// };
+
+// node* traverse( node* head){
+//     node* temp = head;
+//     while( temp!=NULL){
+//         cout<< temp->data<<" ";
+//         temp = temp-> next;
+//     }
+// }
+
+// void reversing_DLL(node* head){
+//     stack<int> st;
+//     node * temp = head;
+//     while( temp != NULL){
+//         st.push(temp->data);
+//         temp = temp -> next;
+//     }
+
+//     temp = head;
+//     while( temp != NULL){
+//         temp -> data = st.top();
+//         st.pop();
+//         temp = temp->next;
+//     }
+// }
+
+// int main(){
+//     node * head = new node;
+//     node * first = new node;
+//     node * second = new node;
+//     node * third = new node;
+
+//     head-> prev = NULL;
+//     head-> data =10;
+//     head-> next = first;
+
+//     first->prev = head;
+//     first-> data =20;
+//     first-> next = second;
+
+//     second->prev = first;
+//     second-> data =30;
+//     second-> next = third;
+
+//     third->prev = second;
+//     third-> data =40;
+//     third-> next = NULL;
+
+//     traverse(head);
+//     cout<<endl;
+//     reversing_DLL(head);
+//     traverse(head);
+
+//     return 0;
+
+// }
+
+
+
+
+// optimal / better solution will use a concept of snapping or we can say it will use
+// changing the DIRECTION OF LINKS AND OUR WORK WILL BE DONE 
+
+
 #include<iostream>
-#include<stack>
 using namespace std;
 
 struct node{
@@ -24,20 +96,18 @@ node* traverse( node* head){
     }
 }
 
-void reversing_DLL(node* head){
-    stack<int> st;
-    node * temp = head;
-    while( temp != NULL){
-        st.push(temp->data);
-        temp = temp -> next;
-    }
+node * ReverseTheDLL ( node * head){
+    node * current = head;
+    node * prev = NULL;
 
-    temp = head;
-    while( temp != NULL){
-        temp -> data = st.top();
-        st.pop();
-        temp = temp->next;
+    while(current!=NULL){
+        prev = current -> prev;
+        current -> prev = current -> next;
+        current -> next = prev;
+
+        current = current -> prev;
     }
+    return prev->prev;
 }
 
 int main(){
@@ -64,9 +134,8 @@ int main(){
 
     traverse(head);
     cout<<endl;
-    reversing_DLL(head);
+    head = ReverseTheDLL(head);
     traverse(head);
-
     return 0;
 
 }
