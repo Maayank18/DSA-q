58888ef1cc2d30f9cfa3b55c4fd20fc06be3c2b7
Maayank18 <maayank51518@gmail.com>
2025-02-08 14:45:50 +0530

    N meetings in the room

--- Greedy Algorithm/N_meeting_inRoom.cpp
+++ Greedy Algorithm/N_meeting_inRoom.cpp
@@ -4,23 +4,46 @@
 using namespace std;
 
 struct data{
-    vector<int> start;
-    vector<int> end;
+    int start;
+    int end;
     int position;
 };
 
-int NumberOfMeetingPossible(vector<int> start, vector<int> end, int n){
-    data arr[n];
+bool comp(data val1, data val2){
+    return val1.end<val2.end;
+}
 
-    for(int i = 0; i<n; i++){
+int NoOfMeeting(vector<int> start, vector<int> end, int N){ // n == size
+    vector<data> arr(N);
+
+    for(int i = 0; i<N; i++){
         arr[i].start = start[i];
         arr[i].end = end[i];
         arr[i].position = i+1;
     }
 
-    bool comp( data val1, data val2){
-        return val1.end<val2.end;
+    sort(arr.begin(),arr.end(),comp);
+
+    int count = 1;
+    int freeTime = arr[0].end;
+    vector<int> FindingPositionsFree;
+
+    for(int i = 1; i<N-1; i++){
+        if(arr[i].start>freeTime){
+            count++;
+            freeTime = arr[i].end;
+            FindingPositionsFree.push_back(arr[i].position);
+        }
     }
 
-    sort(arr,arr+n,comp);
+    return count;
+}
+
+int main(){
+    int n = 6;
+   vector<int> start = {1,3,0,5,8,5};
+   vector<int> end = {2,4,5,7,9,9};
+   int maxMeet = NoOfMeeting(start, end, n);
+   cout<<" possible meetings are : "<<maxMeet<<endl;
+   return 0;
 }
