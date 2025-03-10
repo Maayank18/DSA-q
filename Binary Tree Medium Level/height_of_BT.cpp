da59eb614e22a2e7fec4f7439c1c83dee7768fd6
Maayank18 <maayank51518@gmail.com>
2025-03-10 23:50:00 +0530

    height of binary tree

---
+++ Binary Tree Medium Level/height_of_BT.cpp
@@ -0,0 +1,60 @@
+// here the current node also will be considered to have a height 
+        //             (A)  <- Level 0
+        //            /   \
+        //          (B)   (C)  <- Level 1
+        //         /   \    \
+        //       (D)   (E)  (F)  <- Level 2
+        //      /   \     \
+        //    (G)   (H)   (I)  <- Level 3
+        //            \
+        //           (J)  <- Level 4
+
+// this particular tree has a hieght of 4 but we are also considering the root node too
+// hence here thre height will be 5:-  A -> B -> D -> H -> J
+
+
+// THIS IS TYPE 1 TO SOLVE WHERE WE CAN USE THE RECURSIVE CODE 
+// Here we wont be having time complexity BUT we are using extra stack
+
+#include<iostream>
+using namespace std;
+
+struct Node{
+    int data;
+    Node * left;
+    Node * right;
+    Node( int value ){
+        data = value;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+int Height_Of_BT(Node * root){
+    if( root == NULL) return 0;
+
+    int leftHeight = Height_Of_BT(root->left);
+    int rightHeight = Height_Of_BT(root->right);
+
+    return 1+max(leftHeight,rightHeight);
+}
+
+int main() {
+    // Creating a sample binary tree
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->left->right->right = new Node(6);
+    root->left->right->right->right = new Node(7);
+    
+    int height = Height_Of_BT(root);
+
+    cout << "Maximum depth of the binary tree: " << height << endl;
+
+    return 0;
+}
+
+
+// SECOND METHOD TO SOLVE USING THE LEVEL WISE METHOD
